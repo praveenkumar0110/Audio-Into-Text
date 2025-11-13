b6759528730fea212e870e1479f759ba521f01ca
epraveenkumar7 <ipraveen.e@gmail.com>
2025-11-13 14:56:42 +0530

    Update index.html title and styles; refine update views.py

--- speech_to_text/views.py
+++ speech_to_text/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from transformers import pipeline
 import tempfile
 
-# Load Hugging Face small Whisper model once (efficient)
 model = pipeline("automatic-speech-recognition", model="openai/whisper-small")
 
 def index(request):
@@ -16,7 +15,7 @@ def index(request):
                 tmp.write(chunk)
             tmp_path = tmp.name
 
-        # Transcribe audio using Hugging Face model
+       
         result =  model(tmp_path, return_timestamps=True)
         text = result["text"]
 
